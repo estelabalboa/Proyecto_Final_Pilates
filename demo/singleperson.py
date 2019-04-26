bb8844fbb8cd631837635a249a437083f4e5732b
Estela Balboa <estela.balboa@gmail.com>
2019-04-26 23:16:17 +0200

    feat: added some human pose samples and forloop to read them and make the predictions

--- demo/singleperson.py
+++ demo/singleperson.py
@@ -1,5 +1,8 @@
+import glob
+
 from matplotlib.pyplot import *
 import matplotlib
+
 matplotlib.use("TKAgg")
 from matplotlib import pyplot as plt
 from util.config import load_config
@@ -13,18 +16,30 @@ cfg = load_config("pose_cfg.yaml")
 sess, inputs, outputs = predict.setup_pose_prediction(cfg)
 
 # Read image from file
-file_name = "image.png"
-image = plt.imread(file_name, format='RGB')
+#file_name = "image.png"
+#image = plt.imread(file_name, format='RGB')
 
-image_batch = data_to_input(image)
+# Read images from a path
+pose_image_resources = "../pose_images/*.jpeg"
 
-# Compute prediction with the CNN
-outputs_np = sess.run(outputs, feed_dict={inputs: image_batch})
-scmap, locref, _ = predict.extract_cnn_output(outputs_np, cfg)
 
-# Extract maximum scoring location from the heatmap, assume 1 person
-pose = predict.argmax_pose_predict(scmap, locref, cfg.stride)
+for images in glob.glob(pose_image_resources):
+    try:
+        image = plt.imread(images, format="RGB")
 
-# Visualise
-visualize.show_heatmaps(cfg, image, scmap, pose)
-visualize.waitforbuttonpress()
+        image_batch = data_to_input(image)
+
+        # Compute prediction with the CNN
+        outputs_np = sess.run(outputs, feed_dict={inputs: image_batch})
+        scmap, locref, _ = predict.extract_cnn_output(outputs_np, cfg)
+
+        # Extract maximum scoring location from the heatmap, assume 1 person
+        pose = predict.argmax_pose_predict(scmap, locref, cfg.stride)
+
+        # Visualise
+        visualize.show_heatmaps(cfg, image, scmap, pose)
+        visualize.waitforbuttonpress()
+    except Exception as e:
+        print(e)
+
+
