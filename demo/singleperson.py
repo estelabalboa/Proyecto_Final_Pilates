a2ab5550b3d207d7d72ebbb0a5c0ea3f7b093d57
Estela Balboa <estela.balboa@gmail.com>
2019-05-15 11:47:31 +0200

    feat: added confusion_matrix for true data and predicted data comparision

--- demo/singleperson.py
+++ demo/singleperson.py
@@ -2,7 +2,8 @@ import csv
 import glob
 from itertools import chain
 from typing import Any, List
-
+import pickle
+from sklearn.metrics import r2_score
 import matplotlib
 import pandas as pd
 import numpy as np
@@ -15,6 +16,7 @@ from matplotlib import pyplot as plt
 from util.config import load_config
 from nnet import predict
 from util import visualize
+from sklearn.model_selection import train_test_split
 from dataset.pose_dataset import data_to_input
 from sklearn.metrics import confusion_matrix
 
@@ -27,17 +29,16 @@ sess, inputs, outputs = predict.setup_pose_prediction(cfg)
 
 # Read images from a path
 # pose_image_resources_rw = "../pose_images/DownwardDog/*.jpeg" # 292 + 64 - Score 0,876
-pose_image_resources ="../pose_images/all/*.jpeg"
+# pose_image_resources = "../pose_images/all/*.jpeg"
 # Uncomment this line and comment line before for development purposes (increase time execution)£
-# pose_image_resources = "../pose_images/acc/*.jpeg" # 26 samples 6 testing set --> Score 0,767 (n_estimators=40, max_depth=20) 0,916
+#
+pose_image_resources = "../pose_images/acc/*.jpeg"  # 26 samples 6 testing set --> Score 0,767 (n_estimators=40, max_depth=20) 0,916
 # pose_image_resources ="../pose_images/all_tree/*.jpeg"
 # Images normalization --> using resize_images.py script
 
 features = []
 picture_name = []
 
-pose_dic = ['downward', 'plank', 'warrior', 'tree']
-boolean_dic = ['right', 'wrong']
 
 # Read all images, call cnn model and make predictions about human main body parts
 for images in glob.glob(pose_image_resources):
@@ -93,46 +94,34 @@ features_df: DataFrame = pd.DataFrame(features)
 features_df.columns = labels
 
 features_df['picture_name'] = picture_name
+
+boolean_dict = {'Right': 1, 'Wrong': 0}
 features_df.loc[features_df['picture_name'].str.contains('Right'), 'is_right'] = 1
 features_df.loc[features_df['picture_name'].str.contains('Wrong'), 'is_right'] = 0
-# features_df['is_right_again_2'] = features_df['picture_name'].apply(lambda x: 1 if features_df['picture_name'].str.contains('Right') else 0)
-# features_df['index'] = picture_name
-# features_df = features_df.set_index('index')
+# features_df['is_right_again_2'] = features_df['picture_name'].apply(lambda x: 1 if features_df['picture_name']\
+# .str.contains('Right') else 0)
 
+poses_dict = {'Downward': 1, 'Plank': 2, 'Tree': 3, 'Warrior': 4}
 features_df.loc[features_df['picture_name'].str.contains('Downward'), 'pose'] = 1
 features_df.loc[features_df['picture_name'].str.contains('Plank'), 'pose'] = 2
 features_df.loc[features_df['picture_name'].str.contains('Tree'), 'pose'] = 3
 features_df.loc[features_df['picture_name'].str.contains('Warrior'), 'pose'] = 4
+features_df.loc[~features_df['picture_name'].str.contains(r'Downward|Plank|Tree|Warrior'), 'pose'] = 5
 
 # features_df.to_csv('prepared_data_499.csv')
 
-# for string in features_df['picture_name']:
-#     if 'downward' in string:
-#         y1.append(1)
-#         print("entra en el caso 1")
-#     elif 'plank' in string:
-#         y1.append(2)
-#         print("entra en el caso 2")
-#     elif 'tree' in string:
-#         y1.append(3)
-#         print("entra en el caso 3")
-#     elif 'warrior' in string:
-#         y1.append(4)
-#         print("entra en el caso 4")
-#     # To label "unknown" poses
-#     # else:
-#         # y1.append(5)
-#         # print("entra en el caso unknown")
-#
-# # Create a column from the list
+# Create a column from the list
 # features_df['y1'] = y1
 # features_df = features_df.append(y1)
 
 pd.set_option('display.max_columns', None)
 display(features_df.head(10))
+
+
+# Guardar las coordenadas y pasarselo al RF o al SVM
 features_df.to_csv('prepared_data_tree.csv')
 
-# # Load dataset
+# Load data
 # data = pd.read_csv(file_name, sep=';')
 #
 # # Display records
@@ -144,7 +133,7 @@ features_df.to_csv('prepared_data_tree.csv')
 #
 # data.info()
 #
-# # Some more additional data analysis
+# # More additional data analysis
 # display(np.round(data.describe()))
 
 # display(features_df.isnull().any())
@@ -152,10 +141,7 @@ features_df.to_csv('prepared_data_tree.csv')
 # plt.show()
 # display(features_pd_df.head(n=3))
 # display(features_pd_df.describe(include="all"))
-# Guardar las coordenadas y pasarselo al RF o al SVM
 
-
-from sklearn.model_selection import train_test_split
 
 # Split the data into features and target label
 # features_df = features_df.drop(['picture_name', 'y0', 'y1', 'x_knee_l', 'y_knee_l', 'error_knee_l', 'x_knee_r', 'y_knee_r', 'error_knee_r'], axis=1)
@@ -170,7 +156,6 @@ features_df = features_df.drop(['picture_name', 'y0', 'y1'], axis=1)
 is_right_raw = features_df['pose']
 features_raw = features_df.drop(['pose'], axis=1)
 
-
 # Split the 'features' and 'income' data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(features_raw,
                                                     is_right_raw,
@@ -181,33 +166,50 @@ X_train, X_test, y_train, y_test = train_test_split(features_raw,
 print("Training set has {} samples.".format(X_train.shape[0]))
 print("Testing set has {} samples.".format(X_test.shape[0]))
 
-
-
-
 # Initialize the randomForest model
 # TODO: ver como se comporta el modelo con error y sin error
 # Import any three supervised learning classification models from sklearn
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 
-# Initialize the three models
-reg = RandomForestRegressor(n_estimators=40, max_depth=20)
-reg.fit(X_train, y_train)
+# Initialize the models
+random_forest_class_model = RandomForestClassifier(n_estimators=40, max_depth=20)
+random_forest_class_model.fit(X_train, y_train)
 
-y_pred = reg.predict(X_test)
+logistic_regression_model = LogisticRegression(random_state=42, solver='sag', multi_class='multinomial', max_iter=10000)
+logistic_regression_model.fit(X_train, y_train)
 
-from sklearn.metrics import r2_score
-sco = reg.score(X_test, y_test)
-R2 = r2_score(y_test, y_pred)
+# Save the model
+
+
+pickle.dumps(logistic_regression_model, open("saved_rfc_model.p", "wb"))
+
+y_pred_rf = random_forest_class_model.predict(X_test)
+y_pred_lr = logistic_regression_model.predict(X_test)
+
+
+sco = random_forest_class_model.score(X_test, y_test)
+R2 = r2_score(y_test, y_pred_rf)
 print("R2", R2)
 print("Score", sco)
+Lr_R2 = r2_score(y_test, y_pred_lr)
+print("Lr_R2", Lr_R2)
 
-# confusion_matrix(y_test, y_pred)
+cm_rf = confusion_matrix(y_test, y_pred_rf)
+display(confusion_matrix(y_test, y_pred_lr))
+
+display(cm_rf)
+
+import seaborn as sn
+
+plt.figure(figsize=(10, 7))
+plt.title('Confusion matrix of the Random Forest classifier')
+ax = plt.subplot()
+ax.set_xlabel('Predicted labels')
+ax.set_ylabel('True labels')
+
+sn.heatmap(cm_rf, xticklabels=poses_dict, yticklabels=poses_dict, annot=True, cbar=False, ax=ax)
 plt.show()
-# X_train=
-# y_train=
-# reg = RandomForestRegressor(min_samples_leaf=9, n_estimators=100)
-# reg.fit(X_train, y_train)
 
 
 def write_csv(position_data, file_name, save_error=True):
